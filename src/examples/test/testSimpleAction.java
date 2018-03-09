28548ec0b09bb5d6077988d19f784414b47e0e57
stone-shih <creekyland@gmail.com>
2018-03-09 15:38:11 +0800

    Add struts action test cases

---
+++ src/examples/test/testSimpleAction.java
@@ -0,0 +1,71 @@
+package examples.test;
+
+import servletunit.struts.MockStrutsTestCase;
+
+import java.util.List;
+
+import org.junit.Assert;
+
+
+public class testSimpleAction  extends MockStrutsTestCase {
+	
+	public  testSimpleAction(String testName) {
+		super(testName);
+	}
+
+	public void testSimpleActionAbbrNotExist() {
+		setRequestPathInfo("/processSimple");
+		addRequestParameter("abbr", "");
+		actionPerform();
+		String e[] = {"errors.abbr.required"};
+		verifyActionErrors(e);
+
+	}
+
+	public void testSimpleActionAbbrNoData() {
+		setRequestPathInfo("/processSimple");
+		addRequestParameter("abbr", "QW");
+		actionPerform();
+		String e[] = {"errors.abbr.nodata"};
+		verifyActionErrors(e);
+
+	}
+	
+	
+	public void testSimpleActionSuccess() {
+		
+		setRequestPathInfo("/processSimple");
+		addRequestParameter("abbr", "EU");
+		actionPerform();
+
+		@SuppressWarnings("unchecked")
+		List<Object> ll = (List<Object>) getRequest().getAttribute("cntryList");
+		Assert.assertNotEquals(0, ll.size());
+		verifyForward("success");
+		
+	}
+	
+	
+
+	public void setUp() {
+		try {
+			super.setUp();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void tearDown() {
+		try {
+			super.tearDown();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+
+	
+	
+}
